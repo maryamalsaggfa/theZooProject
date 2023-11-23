5481cff42068a153c58b9eacf303ff5418c395dc
maryamalsaggfa <maryam1xsa@gmail.com>
2023-11-23 00:19:09 -0800

    Initial Commit

---
+++ theZooProject/theZooProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  theZooProjectApp.swift
+//  theZooProject
+//
+//  Created by maryam on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct theZooProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
