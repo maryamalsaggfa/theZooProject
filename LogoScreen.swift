085e338069d3eeef170fa4d5c363af4ed3583837
fayzah <ffowz110@hotmail.com>
2023-11-26 10:25:14 +0300

    logo

---
+++ LogoScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LogoScreen.swift
+//  theZooProject
+//
+//  Created by Faizah Almalki on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct LogoScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoScreen()
+}
